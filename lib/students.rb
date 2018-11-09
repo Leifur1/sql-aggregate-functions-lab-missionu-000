ce2004769ffcb92b5b4d56548f8db57c6e218dab
Leifur Sigurdsson <leifurerik@gmail.com>
2018-11-09 01:19:44 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -5,13 +5,18 @@ def highest_student_gpa
 end
 
 def lowest_student_gpa
+  min(gpa)
 end
 
 def average_student_gpa
+  avg(gpa)
 end
 
 def total_tardies_for_all_students
+  sum(tardies)
 end
 
 def average_gpa_for_9th_grade
+  avg(gpa)
+  where grade = "9"
 end
